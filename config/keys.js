5eb48dee5b87ce4d9fd058d103839c63fc526480
Gevorg Nazaretyan <gevorg.nazaretyan.357@my.csun.edu>
2018-12-19 11:24:44 -0800

    validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://gevorgn:george23@ds113522.mlab.com:13522/mern-stack'
+    mongoURI: 'mongodb://gevorgn:george23@ds113522.mlab.com:13522/mern-stack',
+    secretOrKey: 'secret'
 }
