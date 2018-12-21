789561f91eb6102e3801bd9d4f5cc845d5dbd169
Gevorg Nazaretyan <gevorg.nazaretyan.357@my.csun.edu>
2018-12-21 13:40:37 -0800

    redux actions & reducers

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,2 @@
-export const GET_ITEMS = 'GET_ITEMS';
-export const ADD_ITEM = 'ADD_ITEM';
-export const DELETE_ITEM = 'DELETE_ITEM';
+export const GET_ERRORS = 'GET_ERRORS';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
