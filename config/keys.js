ac4132fa6bbebcdaaf219a9ae86fc72109dcb4af
Gevorg Nazaretyan <gevorg.nazaretyan.357@my.csun.edu>
2019-04-02 12:18:15 -0700

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://gevorgn:george23@ds113522.mlab.com:13522/mern-stack',
-    secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
